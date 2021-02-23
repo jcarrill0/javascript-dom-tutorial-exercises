c950033553b52096a55d685885468c0a2d3809a9
unknown <jcarrillo@SJ-STA-001-L349.clarocr.americamovil.ca1>
2021-02-23 11:59:43 -0600

    Ejercicios terminados

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+/*document.getElementById('inputFirst').addEventListener('keydown', inputCharacters);
+
+function inputCharacters(event) {
+ 
+  if (event.keyCode == 13) {
+    document.getElementById('inputSecond').focus();
+  }
+
+}*/
+
+// VARIABLES
+const todo = document.querySelector('#addToDo');
+const ul = document.getElementsByTagName('UL')[0];
+
+todo.addEventListener('keydown', addTask);
+
+function addTask(e) {
+    if (e.keyCode == 13) {
+        if(e.target.value !== '') {
+            let li = document.createElement('LI');
+            li.innerHTML = `
+                <span><i class="fa fa-trash"></i></span>
+                ${e.target.value} 
+            `;
+            ul.appendChild(li);
+            todo.value='';
+        }
+    }
+}
+
+ul.addEventListener('click', deleteTask);
+
+function deleteTask(e) {
+    if(e.target.classList.contains('fa')) {
+        const tarea = e.target.parentNode.parentNode;
+        tarea.remove();
+    }
+}
